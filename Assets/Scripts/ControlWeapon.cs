93521720b5519afae49741011bbfbce7c3869414
Rmulorm <romulorennerm@yahoo.com.br>
2016-06-04 02:24:08 -0300

    ADDED: Initial Files
    Just a sprite that is moved with wasd, shoot bullets with arrows, but
    with massive stuff still to be developed and improved

---
+++ Assets/Scripts/ControlWeapon.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControlWeapon : MonoBehaviour {
+
+	public bool is_WeaponQuick = false;
+	public Object Bullet;
+	public float BulletSpeed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Vector2 ShootDirection = new Vector2 (Input.GetAxis ("Fire1"), Input.GetAxis("Fire2"));
+
+/*		if (is_WeaponQuick) {
+			
+			is_Shooting = Input.GetAxis ("Fire1");
+		} else {
+
+			is_Shooting = Input.GetButtonDown ("Fire1");
+		}
+*/
+		if (Input.GetButtonDown ("Fire'") || Input.GetButtonDown("Fire2"))
+		if (ShootDirection != new Vector2 (0,0)) {
+
+			ShootBullet (ShootDirection);
+		}
+	}
+
+	void ShootBullet (Vector2 BulletDirection) {
+
+		GameObject ShootedBullet;
+
+		ShootedBullet = (GameObject)Instantiate(Bullet, transform.position, transform.rotation);
+		ShootedBullet.SetActive (true);
+		ShootedBullet.GetComponent<Rigidbody2D> ().AddForce(BulletSpeed * BulletDirection);
+	}
+}
---
+++ Assets/Scripts/MainPlayerControler.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainPlayerControler : MonoBehaviour {
+
+	public float f_speed;
+	public int i_life;
+	public int i_damage;
+
+	private Rigidbody2D r2_body;
+
+
+	// Use this for initialization
+	void Start () {
+
+		r2_body = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void FixedUpdate () {
+
+		float f_MoveHorizontal = Input.GetAxis ("Horizontal");
+
+		float f_MoveVertical = Input.GetAxis ("Vertical");
+
+		Vector2 v2_Move = new Vector2 (f_MoveHorizontal, f_MoveVertical);
+
+		r2_body.AddForce (v2_Move * f_speed);
+	}
+}
